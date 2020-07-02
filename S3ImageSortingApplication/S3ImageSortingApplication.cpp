59ee4536e884388b9ca7fe43bbf96db7cc364adc
DetailsMatterM <moritz.denke@outlook.com>
2020-07-02 16:18:43 +0200

    create directory implementation

--- S3ImageSortingApplication/S3ImageSortingApplication.cpp
+++ S3ImageSortingApplication/S3ImageSortingApplication.cpp
@@ -8,6 +8,9 @@
 #include <strsafe.h>
 #include <string>
 #include "atlstr.h"
+#include <io.h>
+#include <direct.h>
+#include <sstream>
 
 #define MAX 60
 
@@ -15,13 +18,40 @@ using namespace std;
 
 TCHAR* convertToTCHAR(char* cha)
 {
-    int aa = strlen(cha);
-    TCHAR* tmp = new TCHAR[aa + 1];
-    for (int i = 0; i < aa + 1; i++)
+    int length = strlen(cha);
+    TCHAR* tmp = new TCHAR[length + 1];
+    for (int i = 0; i < length + 1; i++)
     {
         tmp[i] = cha[i];
     }
     return tmp;
+}
+
+wstring s2ws(const std::string& s)
+{
+    int len;
+    int slength = (int)s.length() + 1;
+    len = MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, 0, 0);
+    wchar_t* buf = new wchar_t[len];
+    MultiByteToWideChar(CP_ACP, 0, s.c_str(), slength, buf, len);
+    std::wstring r(buf);
+    delete[] buf;
+    return r;
+}
+
+string stringAdjust(string input) {
+    stringstream stream;
+
+    for (int i = 0; i < input.length(); ++i) {
+        if (input[i] == '\\') {
+            stream << "\\\\";
+        }
+        else {
+            stream << input[i];
+        }
+    }
+    string output = stream.str();
+    return output;
 }
 
 int main(int argc, char* argv[])
@@ -56,12 +86,42 @@ int main(int argc, char* argv[])
         printf("FindFirstFile failed (%d)\n", GetLastError());
         return (-1);
     }
+    string newPath = argv[1];
+    cout << newPath << endl;
+    newPath = stringAdjust(newPath);
+
+    //newPath = newPath.replace(newPath.begin(), newPath.end(), '\\', '\\\\');
+    /*for (int i = 0; i < newPath.length(); i++) {
+        if (newPath[i] == '\\') {
+            newPath.insert(i - 1, 1, '\\');
+        }
+    }
+    */
+    cout << newPath << endl;
+
+    newPath.append("\\Oktober");
+    wstring stemp = s2ws(newPath);
+    LPCWSTR pathToUse = stemp.c_str();
+    CreateDirectory(pathToUse, NULL);
 
     do {
         PTSTR filename = FoundFileData.cFileName;
         size_t size = wcstombs(NULL, filename, 0);
         char* CharStr = new char[size + 1];
         wcstombs(CharStr, filename, size + 1); //Access to filename works for example like CharStr[15]
+        if (filename[4] == '1') {
+            switch (filename[5]) {
+            case 0:
+                
+                string newPath = argv[1];
+                newPath.append("Oktober");
+                wstring stemp = s2ws(newPath);
+                LPCWSTR pathToUse = stemp.c_str();
+                CreateDirectory(pathToUse, NULL);
+                
+
+            }
+        }
         _tprintf(TEXT("  %s \n"), FoundFileData.cFileName);
     } while (FindNextFile(hFind, &FoundFileData) != 0);
 
